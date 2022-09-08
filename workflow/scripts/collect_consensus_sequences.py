e2a8571c77da48d69b689db220fbc78de17ca4a0
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-09-08 17:32:32 +0200

    fix indent

--- workflow/scripts/collect_consensus_sequences.py
+++ workflow/scripts/collect_consensus_sequences.py
@@ -4,24 +4,23 @@ from Bio.SeqRecord import SeqRecord
 
 def main(fnames_consensus, fname_merged_out):
 
+    with open(fname_merged_out, 'w') as out_f:
+        my_seqs = []
+        for fname_consensus in fnames_consensus:
+            experiment = fname_consensus.split("/")[-5]
+            patient = fname_consensus.split("/")[-4]
+            date = fname_consensus.split("/")[-3]
 
-with open(fname_merged_out, 'w') as out_f:
-    my_seqs = []
-    for fname_consensus in fnames_consensus:
-        experiment = fname_consensus.split("/")[-5]
-        patient = fname_consensus.split("/")[-4]
-        date = fname_consensus.split("/")[-3]
+            print('experiment ', experiment)
+            print('patient ', patient)
+            print('date ', date)
 
-        print('experiment ', experiment)
-        print('patient ', patient)
-        print('date ', date)
+            for record in SeqIO.parse(fname_consensus, 'fasta'):
 
-        for record in SeqIO.parse(fname_consensus, 'fasta'):
+                new_seq = str(record.seq)
+                my_seqs.append(SeqRecord(Seq(new_seq), id = experiment+'-'+patient'-'+date))
 
-            new_seq = str(record.seq)
-            my_seqs.append(SeqRecord(Seq(new_seq), id = experiment+'-'+patient'-'+date))
-
-    SeqIO.write(my_seqs, out_f, "fasta")
+        SeqIO.write(my_seqs, out_f, "fasta")
 
 
 if __name__ == "__main__":
