3d4a6b129ad6d4c3dc0bb09b4bd72cb6e536d2de
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-10-13 20:34:06 +0200

    add script to shift mutation calls to EB ref space

--- workflow/scripts/transfrom_mutations_to_EB_space.py
+++ workflow/scripts/transfrom_mutations_to_EB_space.py
@@ -3,10 +3,10 @@
 import pandas as pd
 
 def f_shift(row):
-    if row['POS'] <= 248:
-        val = row['POS']+25
-    elif row['POS'] >= 250:
-        val = row['POS']+24
+    if row['Pos'] <= 248:
+        val = row['Pos']+25
+    elif row['Pos'] >= 250:
+        val = row['Pos']+24
     return val
 
 def main(fname_all_mutations, fname_all_mutations_shifted):
@@ -16,8 +16,8 @@ def main(fname_all_mutations, fname_all_mutations_shifted):
     sample = str(fname_all_mutations).split("/")[-1]
 
     if sample != "parental_stock_ref_EBref":
-        df_all_muts = df_all_muts[df_all_muts['POS']!=249]
-        df_all_muts['POS'] = df_all_muts.apply(f_shift, axis=1)
+        df_all_muts = df_all_muts[df_all_muts['Pos']!=249]
+        df_all_muts['Pos'] = df_all_muts.apply(f_shift, axis=1)
 
     df_all_muts.to_csv(fname_all_mutations_shifted)
 
