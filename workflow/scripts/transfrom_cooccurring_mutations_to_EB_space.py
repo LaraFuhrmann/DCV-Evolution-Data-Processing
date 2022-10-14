db11583574fff025f5cae7c5bd790da7a9a0d8c9
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-10-14 11:32:24 +0200

    cooccurring mutations in EB ref space

---
+++ workflow/scripts/transfrom_cooccurring_mutations_to_EB_space.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def f_shift(row):
+    if row['position'] <= 248:
+        val = row['position']+25
+    elif row['position'] >= 250:
+        val = row['position']+24
+    else:
+        val = 0
+    return val
+
+def main(fname_all_mutations, fname_all_mutations_shifted):
+
+    df_all_muts = pd.read_csv(fname_all_mutations)
+
+    sample = str(fname_all_mutations).split("/")[-1]
+
+    if sample != "parental_stock_ref_EBref":
+        df_all_muts['position_EB_space'] = df_all_muts.apply(f_shift, axis=1)
+
+    df_all_muts.to_csv(fname_all_mutations_shifted)
+
+if __name__ == "__main__":
+    main(
+        snakemake.input.fname_all_mutations,
+        snakemake.output.fname_all_mutations_shifted,
+    )
