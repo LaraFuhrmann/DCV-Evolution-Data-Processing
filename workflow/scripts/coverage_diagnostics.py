7bea33bd601bd2365e6c23fb053ff72be4dec286
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-11-01 09:39:16 +0100

    exception for parental_stock

--- workflow/scripts/coverage_diagnostics.py
+++ workflow/scripts/coverage_diagnostics.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 
 def main(file_list, fname_coverage_all, fname_coverage_plt):
 
@@ -29,52 +32,64 @@ def main(file_list, fname_coverage_all, fname_coverage_plt):
 
     df.to_csv(fname_coverage_all)
 
+    if cell_line == "parental_stock":
+        plt.plot(df['pos'].values, df['coverage'].values)
+        plt.plot(1000*np.ones(len(df_temp['pos'].values)), color='red')
+        plt.set_xticks(np.arange(0, len(df['pos'].values)+1, 1000))
+        plt.xaxis.set_tick_params(rotation=70)
+        plt.savefig(fname_coverage_plt,format='pdf')
 
-    # plotting for each cell_line and passage separatly.
-    import matplotlib
-    matplotlib.use('Agg')
-    from matplotlib import pyplot as plt
-
-    passages = ['passage_1', 'passage_5', 'passage_10']
-    replicates = ['replicate_a', 'replicate_b', 'replicate_c', 'replicate_d', 'replicate_e']
-
-    fig, ax = plt.subplots(5,3, figsize=(20, 15),constrained_layout=True, sharey=True,  sharex=True)
-    fig.suptitle('Coverage along the genome', fontsize=20)
+        plt.plot(df['pos'].values, df['coverage'].values)
+        plt.plot(1000*np.ones(len(df_temp['pos'].values)), color='red')
+        plt.set_xticks(np.arange(0, len(df['pos'].values)+1, 1000))
+        plt.xaxis.set_tick_params(rotation=70)
+        plt.set_yscale('log')
+        plt.savefig(fname_coverage_plt+'.log.pdf',format='pdf')
 
 
-    for idx_rep, rep in enumerate(replicates):
-        for idx_vp, vp in enumerate(passages):
-            df_temp = df[df['cell_line'] == rep]
-            df_temp = df_temp[df_temp['passage']==vp]
+    else:
+        # plotting for each cell_line and passage separatly.
 
-            ax[idx_rep][idx_vp].set_xticks(np.arange(0, len(df_temp['pos'].values)+1, 1000))
-            ax[idx_rep][idx_vp].xaxis.set_tick_params(rotation=70)
-            ax[idx_rep][idx_vp].plot(df_temp['pos'].values, df_temp['coverage'].values)
+        passages = ['passage_1', 'passage_5', 'passage_10']
+        replicates = ['replicate_a', 'replicate_b', 'replicate_c', 'replicate_d', 'replicate_e']
 
-            ax[idx_rep][idx_vp].set_title(vp)
-            ax[idx_rep][idx_vp].set_ylabel(rep)
+        fig, ax = plt.subplots(5,3, figsize=(20, 15),constrained_layout=True, sharey=True,  sharex=True)
+        fig.suptitle('Coverage along the genome', fontsize=20)
 
-            ax[idx_rep][idx_vp].plot(1000*np.ones(len(df_temp['pos'].values)), color='red')
-            #ax[idx_rep][idx_vp].set_yscale('log')
 
-    plt.savefig(fname_coverage_plt,format='pdf')
+        for idx_rep, rep in enumerate(replicates):
+            for idx_vp, vp in enumerate(passages):
+                df_temp = df[df['cell_line'] == rep]
+                df_temp = df_temp[df_temp['passage']==vp]
 
-    for idx_rep, rep in enumerate(replicates):
-        for idx_vp, vp in enumerate(passages):
-            df_temp = df[df['cell_line'] == rep]
-            df_temp = df_temp[df_temp['passage']==vp]
+                ax[idx_rep][idx_vp].set_xticks(np.arange(0, len(df_temp['pos'].values)+1, 1000))
+                ax[idx_rep][idx_vp].xaxis.set_tick_params(rotation=70)
+                ax[idx_rep][idx_vp].plot(df_temp['pos'].values, df_temp['coverage'].values)
 
-            ax[idx_rep][idx_vp].set_xticks(np.arange(0, len(df_temp['pos'].values)+1, 1000))
-            ax[idx_rep][idx_vp].xaxis.set_tick_params(rotation=70)
-            ax[idx_rep][idx_vp].plot(df_temp['pos'].values, df_temp['coverage'].values)
+                ax[idx_rep][idx_vp].set_title(vp)
+                ax[idx_rep][idx_vp].set_ylabel(rep)
 
-            ax[idx_rep][idx_vp].set_title(vp)
-            ax[idx_rep][idx_vp].set_ylabel(rep)
+                ax[idx_rep][idx_vp].plot(1000*np.ones(len(df_temp['pos'].values)), color='red')
+                #ax[idx_rep][idx_vp].set_yscale('log')
 
-            ax[idx_rep][idx_vp].plot(1000*np.ones(len(df_temp['pos'].values)), color='red')
-            ax[idx_rep][idx_vp].set_yscale('log')
+        plt.savefig(fname_coverage_plt,format='pdf')
 
-    plt.savefig(fname_coverage_plt+'.log.pdf',format='pdf')
+        for idx_rep, rep in enumerate(replicates):
+            for idx_vp, vp in enumerate(passages):
+                df_temp = df[df['cell_line'] == rep]
+                df_temp = df_temp[df_temp['passage']==vp]
+
+                ax[idx_rep][idx_vp].set_xticks(np.arange(0, len(df_temp['pos'].values)+1, 1000))
+                ax[idx_rep][idx_vp].xaxis.set_tick_params(rotation=70)
+                ax[idx_rep][idx_vp].plot(df_temp['pos'].values, df_temp['coverage'].values)
+
+                ax[idx_rep][idx_vp].set_title(vp)
+                ax[idx_rep][idx_vp].set_ylabel(rep)
+
+                ax[idx_rep][idx_vp].plot(1000*np.ones(len(df_temp['pos'].values)), color='red')
+                ax[idx_rep][idx_vp].set_yscale('log')
+
+        plt.savefig(fname_coverage_plt+'.log.pdf',format='pdf')
 
 
 if __name__ == "__main__":
