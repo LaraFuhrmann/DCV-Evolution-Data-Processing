11b10367e6317ff4affa2c02478c4e799f1a20af
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2022-12-16 15:18:04 +0100

    download forward and reverse reads

--- resources/samples/mongelli2022/download_samples.sh
+++ resources/samples/mongelli2022/download_samples.sh
@@ -3,5 +3,5 @@ cd ./resources/samples/mongelli2022/
 mkdir replicate_a
 cd replicate_a
 
-fastq-dump -O passage_1/raw_data SRR17044623
-fastq-dump -O passage_10/raw_data SRR17044490
+fastq-dump -O passage_1/raw_data --split-e SRR17044623
+fastq-dump -O passage_10/raw_data --split-e SRR17044490
