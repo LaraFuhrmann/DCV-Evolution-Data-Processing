2784b459100e7f488c8b2f49997a659bfa76da6f
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-06-01 16:08:46 +0200

    add hsv data

--- resources/deletion_analysis/download_samples.sh
+++ resources/deletion_analysis/download_samples.sh
@@ -6,4 +6,6 @@ fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/wol_P3/raw_da
 
 fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/mock_P3/raw_data/ ERR3510616
 
-fastq-dump -O resources/samples/deletion_analysis-hsv/obbard2015/Dmel_EIKST/raw_data/ SRR1914412
+fastq-dump -O resources/samples/deletion_analysis_dcv/obbard2015/Dmel_EIKST/raw_data/ SRR1914412
+
+fastq-dump -O resources/samples/deletion_analysis_hsv/munoz2021/XX/raw_data/ ERR3278849
