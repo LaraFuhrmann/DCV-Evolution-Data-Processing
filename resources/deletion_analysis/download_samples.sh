c7ebd92cb9c576e4e529f09cd795d53d46d57428
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-06-01 15:52:35 +0200

    dont split raw reads in forward and reverse

--- resources/deletion_analysis/download_samples.sh
+++ resources/deletion_analysis/download_samples.sh
@@ -1,9 +1,9 @@
-fastq-dump -O resources/samples/deletion_analysis_dcv/Longdon2018/Dmel_10/raw_data/ --split-e SRR17044453
+fastq-dump -O resources/samples/deletion_analysis_dcv/Longdon2018/Dmel_10/raw_data/  SRR17044453
 
 fastq-dump -O resources/samples/deletion_analysis_dcv/Longdon2018/Dpro_10/raw_data/ SRR6156525
 
-fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/wol_P3/raw_data/ --split-e ERR3510613
+fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/wol_P3/raw_data/ ERR3510613
 
-fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/mock_P3/raw_data/ --split-e ERR3510616
+fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/mock_P3/raw_data/ ERR3510616
 
-fastq-dump -O resources/samples/deletion_analysis-hsv/obbard2015/Dmel_EIKST/raw_data/ --split-e SRR1914412
+fastq-dump -O resources/samples/deletion_analysis-hsv/obbard2015/Dmel_EIKST/raw_data/ SRR1914412
