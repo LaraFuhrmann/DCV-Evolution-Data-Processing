aede0723c92c47539e63dd8ce89cda3957fa24ca
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-06-02 10:09:00 +0200

    download samples

--- resources/deletion_analysis/download_samples.sh
+++ resources/deletion_analysis/download_samples.sh
@@ -8,7 +8,6 @@ fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/mock_P3/raw_d
 
 fastq-dump -O resources/samples/deletion_analysis_dcv/obbard2015/Dmel_EIKST/raw_data/ SRR1914412
 
-
 fastq-dump -O resources/samples/deletion_analysis_dcv/mongelli2022/S2_P0/raw_data/  SRR17044471
 
 fastq-dump -O resources/samples/deletion_analysis_dcv/mongelli2022/dcrKO_P10/raw_data/  SRR17044453
