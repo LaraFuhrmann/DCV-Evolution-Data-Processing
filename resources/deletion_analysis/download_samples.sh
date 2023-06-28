e2b36a468d199685f248dd5396d171ce572fd64a
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-06-28 11:13:57 +0200

    Deletion analysis (#1)
    * file for obbard2015
    
    * file for Mongelli2022 and Longdon2018
    
    * samples files
    
    * first clean-up to make deletion analysis reproducible
    
    * snakefile general
    
    * snakefile general
    
    * add general config
    
    * correct paths
    
    * add last things
    
    * add
    
    * add this
    
    * correct output direcotyr
    
    * dont split raw reads in forward and reverse
    
    * correct paths
    
    * add ref
    
    * add hsv data
    
    * add hsv data
    
    * add mongelli samples
    
    * download samples
    
    * correct space
    
    * add reference name
    
    * [add] notebooks for manuscript figures
    
    * add another mongelli sample to process
    
    ---------
    
    Co-authored-by: Oscar Lezcano <oscar.morales.lezcano@gmail.com>

---
+++ resources/deletion_analysis/download_samples.sh
@@ -0,0 +1,19 @@
+fastq-dump -O resources/samples/deletion_analysis_dcv/Longdon2018/Dmel_10/raw_data/  SRR17044453
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/Longdon2018/Dpro_10/raw_data/ SRR6156525
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/wol_P3/raw_data/ ERR3510613
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/martinez2019/mock_P3/raw_data/ ERR3510616
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/obbard2015/Dmel_EIKST/raw_data/ SRR1914412
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/mongelli2022/S2_P0/raw_data/  SRR17044471
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/mongelli2022/dcrKO_P10/raw_data/  SRR17044453
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/mongelli2022/WT_p1/raw_data/  SRR17044623
+
+fastq-dump -O resources/samples/deletion_analysis_hsv/munoz2021/hsv2_wt/raw_data/ --split-3 ERR3278849
+
+fastq-dump -O resources/samples/deletion_analysis_dcv/mongelli2022/WT_p10/raw_data/ SRR17044490 # WT repa P10
