154af8605862f96144db2e78802b06e145785433
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-11-02 12:45:01 +0100

    nested anova to compare rna copies among parental stock and p10 evolved populations

--- workflow/notebooks/Figure_2E_stats_rna_copies.R
+++ workflow/notebooks/Figure_2E_stats_rna_copies.R
@@ -1,21 +1,30 @@
+# Using a nested ANOVA to account for the technical replicates, we are comparing 
+# the RNA copies of the viral populations parental stock to the passage 10 evolved 
+# virues in the three fly genotypes. 
 
 # Load data
 base_path = "/Users/lfuhrmann/Documents/Projects/DCV-Evolution/DCV-Evolution-Data-Processing/workflow/notebooks"
 df <- read.csv(paste(base_path,"/RNA_copies_for_R_script.csv", sep = ""))
 
-# exlcude passage 1
-#df <- df[!df$passage==1,]
+#exlcude passage 1
+df <- df[!df$passage==1,]
 
 df$RNA_copies_log <- log(df$RNA_copies)
 
+df$replicate <- paste(df$genotype, df$biological_replicate)
+#df$id <- paste(df$genotype,  df$biological_replicate)
+
 #fit nested ANOVA
-model_formula <- RNA_copies_log ~factor(genotype) * factor(passage) * factor(biological_replicate)  / factor(Technical.replicate)
-nest <- aov(data=df,model_formula)
+#nested_repeated_formula <- RNA_copies_log ~ factor(genotype) * factor(passage)*factor(replicate)  / factor(Technical.replicate) + Error(factor(id)/factor(passage))
+nested_formula <- RNA_copies_log ~ factor(genotype) * factor(replicate)  / factor(Technical.replicate)
+nest <- aov(data=df,nested_formula)
 
 #view summary of nested ANOVA
 summary(nest)
 
-tuk<-tukey_hsd(nest, which="factor(genotype):factor(passage)")
+library(rstatix)
+tuk<-tukey_hsd(nest, which="factor(genotype)")
+tuk
 
 ### check ANOVA assumptions
 # check if residuals follow approximalty normal distribution 
@@ -25,4 +34,4 @@ qqline(rstandard(nest))
 
 #conduct Levene's Test for equality of variances
 library(car)
-leveneTest(model_formula, data=df)
+leveneTest(nested_formula, data=df)
