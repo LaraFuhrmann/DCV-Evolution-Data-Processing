2c6bdf7da8eabfda4e899eaf67cb35b14a22ae7d
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-05-03 10:26:34 +0200

    update

--- resources/host_reads_fraction/resources/samples/download_samples.sh
+++ resources/host_reads_fraction/resources/samples/download_samples.sh
@@ -5,4 +5,4 @@ module load sra-toolkit/2.10.5
 # ------ Download samples
 fastq-dump -O OE/OEa10/raw_data --split-e  SRR25258487
 fastq-dump -O WT/WTd10/raw_data --split-e  SRR25258490
-fastq-dump -O KO/KOc1/raw_data --split-e  SRR25258500	
+fastq-dump -O KO/KOc1/raw_data --split-e  SRR25258500
